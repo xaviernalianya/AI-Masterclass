796169abe7ece47128d395a72bb6952f685e4d18
Xavier Nalianya <xaviernalianya@gmail.com>
2026-08-22 06:59:57 +0300

    commit

--- week3/day4.py
+++ week3/day4.py
@@ -11,5 +11,16 @@ people=[
     {"name":"Martha","steps":[9000,8000,7000,6000,11000,4000,8990]},
     {"name":"Tomas","steps":[8877,2829,9884,6783,7878,6457,9000]},
     {"name":"Xavy","steps":[9000,8000,7000,6000,11000,4000,8990]},
+]
 
-# 1 Building steps counts above 10,000 
+# 1 Building step counts above 10,000
+all_steps = [s for p in people for s in p["steps"]]
+high_steps = [s for s in all_steps if s > 10000]
+print("Steps above 10,000:", high_steps)
+
+# 2 Building names of people whose weekly average is above 9,000
+names_above_average = [
+    p["name"] for p in people
+    if sum(p["steps"]) / len(p["steps"]) > 7000
+]
+print("People averaging above 9,000 steps:", names_above_average)
