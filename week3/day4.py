9c6f593adf5d856768db79f80de6ecafc7bf653d
Xavier Nalianya <xaviernalianya@gmail.com>
2026-08-22 06:40:00 +0300

    commit

---
+++ week3/day4.py
@@ -0,0 +1,15 @@
+#Exercise
+#You have a list of weekly step counts for four different people. 
+# Use list comprehensions to:
+#  (1) build a list of all step counts that are above 10,000 from any person, and
+#  (2) build a list of names of people whose average steps for the week is above 9,000.
+
+
+people=[
+    {"name":"James","steps":[7000,9000,7600,1200,8000,8700,9900]},
+    {"name":"John","steps":[8877,2829,9884,6783,7878,6457,9000]},
+    {"name":"Martha","steps":[9000,8000,7000,6000,11000,4000,8990]},
+    {"name":"Tomas","steps":[8877,2829,9884,6783,7878,6457,9000]},
+    {"name":"Xavy","steps":[9000,8000,7000,6000,11000,4000,8990]},
+
+# 1 Building steps counts above 10,000 
