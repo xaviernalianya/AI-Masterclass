177ebd5ad4533e7f7f20253d6699e5cff115e6ea
Xavier Nalianya <xaviernalianya@gmail.com>
2026-08-25 06:51:25 +0300

    25

--- week4/day1.py
+++ week4/day1.py
@@ -13,10 +13,30 @@ import io
 
 # Simulating file write using in-memory buffer
 file_content = io.StringIO()
-file_content.write("Steps: 9200\n")
+file_content.write("Steps: 9000\n")
 file_content.write("Water: 8 glasses\n")
 file_content.write("Protocol: OMAD\n")
 file_content.write("Cold shower: Yes\n")
 
 print("File written. Contents:")
 print(file_content.getvalue())
+
+#To read a file in Python
+with open("daily_log.txt", "r") as f:
+    content=f.read()
+    print(content)
+
+import io
+file_data="""Steps:9200
+    Water: 8 glasses
+    Protocol: OMAD
+    Cold shower: YES
+    Sleep hours: 7.5
+"""
+f=io.StringIO(file_data)
+lines=f.readlines()
+print("No. of line ", len(lines))
+
+for l in lines:
+    l=l.strip() 
+    print(l)
