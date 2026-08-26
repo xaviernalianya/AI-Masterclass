1d549b1bac963a909b8623437dadec1d13d8e107
Xavier Nalianya <xaviernalianya@gmail.com>
2026-08-26 09:55:44 +0300

    266

--- week4/day2.py
+++ week4/day2.py
@@ -30,4 +30,23 @@ except ValueError as e:
     print("ValueError:", e)
 except TypeError as e:
     print("TypeError:", e)
-    
+
+
+#Write a function called safe_log_entry(data) that takes a dictionary and
+#  tries to extract steps, water, and protocol. 
+# If steps is not a valid integer, print an error and return None. 
+# If water is missing, use a default of 0. 
+# If protocol is missing, use "Unknown". Print a clean report for each valid entry.
+
+
+def safe_log_entry(data):[
+    {"steps":5000, "water": 8, "protocol":"OMAD"},
+    {"steps":5000, "water": 8, "protocol":"OMAD"},
+    {"steps":5000, "water": 8, "protocol":"OMAD"},
+    ]
+try:
+   steps= int(data["steps"])
+except(ValueError, TypeError, KeyError):
+    print("Invalid steps data. Skipping entry")
+    return None
+                      
